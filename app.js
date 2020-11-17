fc1101c197cde45e40020e827878c47539ce6044
Kay <kahachang@gmail.com>
2020-11-17 17:15:43 +0800

    add axios & express-handlebars

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 const express = require('express')
-const request = require('request')
+const axios = require('axios')
 const cheerio = require('cheerio')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
-// 測試 soft-job 版爬蟲
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 const pttUrl = 'https://www.ptt.cc'
-request({
-  url: `${pttUrl}/bbs/Soft_Job/index.html`
-}, (err, res, body) => {
-  if (err || !body) {
-    return
-  }
-  let result = []
-  const $ = cheerio.load(body)
-  const list = $('.r-list-container .r-ent')
-  for (let i = 0; i < list.length - 4; i++) {
-    const title = list.eq(i).find('.title a').text()
-    const author = list.eq(i).find('.author').text()
-    const date = list.eq(i).find('.date').text()
-    const link = list.eq(i).find('a').attr('href')
-    const url = pttUrl + link
-
-    result.push({ title, author, date, url })
-  }
-
-  console.log(result)
-})
 
 app.get('/', (req, res) => {
-  res.send('the app is running')
+  axios.get(`${pttUrl}/bbs/Soft_Job/index.html`)
+    .then((infos) => {
+      // console.log(infos.data)
+      let result = []
+      const $ = cheerio.load(infos.data)
+      const list = $('.r-list-container .r-ent')
+      for (let i = 0; i < list.length - 4; i++) {
+        const title = list.eq(i).find('.title a').text()
+        const author = list.eq(i).find('.author').text()
+        const date = list.eq(i).find('.date').text()
+        const link = list.eq(i).find('a').attr('href')
+        const url = pttUrl + link
+
+        result.push({ title, author, date, url })
+        console.log(result)
+      }
+      res.render('index', { result })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 app.listen(PORT, () => {
